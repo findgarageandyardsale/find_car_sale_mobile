1e9c8037fc434bdea3f9dc2669958e5d5d1fc1ed
Anu Prajapati <anuprajapati@Anus-MacBook-Air.local>
2025-09-24 09:33:42 +0545

    Google service

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,6 +6,8 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+
+    id("com.google.gms.google-services")
 }
 
 // Load keystore properties
@@ -81,6 +83,8 @@ flutter {
 
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+    implementation(platform("com.google.firebase:firebase-bom:34.3.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
 
 
