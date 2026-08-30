20206364a2ad351f7b1fb1bd80dcce91148c063a
Susan Timilsina <susantimilsina10@gmail.com>
2026-08-30 18:09:12 +0545

    Switch API base URL to production domain
    - http://52.62.167.49:8000 → https://api.findcarsales.com
    - /garage/yard/payment/ → /find/car/sale/payment/
    - /garage/yard/rating/ → /find/carsale/rating/

--- lib/configs/app_configs.dart
+++ lib/configs/app_configs.dart
@@ -1,8 +1,5 @@
 class AppConfigs {
-  // http://54.146.185.244/api/v1/find/car/sales/
-  static String url = 'http://54.146.185.244:8000';
-  // static String url = 'http://127.0.0.1:8000';
-  ///http://127.0.0.1:8000/api/v1/home/user/login/
+  static String url = 'https://api.findcarsales.com';
   static String baseUrl = '$url/api/v1';
 
   static String loginEndpoint = '/home/user/login/';
@@ -18,13 +15,13 @@ class AppConfigs {
 
   static String changePasswordEndpoint = '/user/changed_password/';
   static String uploadAttachment = '/attachment/save_attachment/';
-  static String postFeedback = '/garage/yard/rating/';
+  static String postFeedback = '/find/carsale/rating/';
 
   static String getUserEndpoint({required String id}) => '/user/$id/';
   static String profileEndpoint = '/user/profile/';
   static String logout = '/home/user/logout/';
   static String createSales = '/find/car/sales/';
-  static String payementSales = '/garage/yard/payment/';
+  static String payementSales = '/find/car/sale/payment/';
   static String getCarCondition = '/find/carsale/conditions/';
 
   static String stripeEndpoint = 'https://api.stripe.com/v1/payment_intents';
